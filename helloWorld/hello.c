914b4eb8eeb4319d73d2f158da1dee8f584af996
Kiaan Castillo <kiaanc@sfu.ca>
2023-01-19 19:24:23 -0800

    Modularize steps of the game

--- helloWorld/hello.c
+++ helloWorld/hello.c
@@ -11,6 +11,14 @@
 #define CORRECT_ANSWER_LED_FLASH_DURATION_MS 500
 #define INCORRECT_ANSWER_LED_FLASH_DURATION_MS 1000
 
+static int fastestTime = 0;
+
+typedef enum
+{
+  START_NEW_ROUND,
+  END_GAME
+} ROUND_RESULT;
+
 void intro(void)
 {
   printf("CMPT 433 | Kiaan Castillo | Assignment 1\n");
@@ -41,6 +49,80 @@ void initializeRound(void)
   turnOnLEDMiddle2();
 }
 
+bool playerPressedTooEarly(void)
+{
+  int waitTimeBeforeRoundStart = getRandomTimeInMs();
+  int roundStartTime = getTimeInMs();
+  bool pressedTooEarly = false;
+
+  do
+  {
+    if (isJoystickUpPressed() || isJoystickDownPressed())
+    {
+      printf("Too soon!\n");
+      printf("\n");
+      pressedTooEarly = true;
+      break;
+    }
+  } while (getTimeInMs() - roundStartTime < waitTimeBeforeRoundStart);
+  return pressedTooEarly;
+}
+
+ROUND_RESULT startRound(void)
+{
+  turnOffAllLEDs();
+  Direction direction = getRandomDirectionUpOrDown();
+  turnOnLED(direction);
+  long long startTime = getTimeInMs();
+
+  do
+  {
+    if ((isJoystickUpPressed() && direction == UP) || (isJoystickDownPressed() && direction == DOWN))
+    {
+      long long responseTime = getTimeInMs() - startTime;
+
+      if (fastestTime == 0)
+      {
+        fastestTime = responseTime;
+      }
+      else if (fastestTime > responseTime)
+      {
+        fastestTime = responseTime;
+        printf("NEW RECORD! ");
+      }
+
+      printf("Response time: %lldms\n", responseTime);
+      printf("\n");
+      correctAnswerLEDFlash();
+      sleepForMs(CORRECT_ANSWER_LED_FLASH_DURATION_MS);
+
+      break;
+    }
+
+    if ((isJoystickUpPressed() && direction != UP) || (isJoystickDownPressed() && direction != DOWN))
+    {
+      printf("Whoops, incorrect.\n");
+      printf("\n");
+      incorrectAnswerLEDFlash();
+      sleepForMs(INCORRECT_ANSWER_LED_FLASH_DURATION_MS);
+
+      break;
+    }
+
+    if (isJoystickLeftPressed() || isJoystickRightPressed())
+    {
+      return END_GAME;
+    }
+  } while (getTimeInMs() - startTime <= MAX_RESPONSE_TIME_IN_MS);
+
+  if (getTimeInMs() - startTime > MAX_RESPONSE_TIME_IN_MS)
+  {
+    return END_GAME;
+  }
+
+  return START_NEW_ROUND;
+}
+
 void endGame(void)
 {
   printf("\n");
@@ -51,83 +133,26 @@ void endGame(void)
 
 int main()
 {
-  int fastestTime = 0;
   initializeGame();
 
   while (1)
   {
     initializeRound();
 
-    turnOffAllLEDs();
-    int waitTimeBeforeRoundStart = getRandomTimeInMs();
-    int roundStartTime = getTimeInMs();
-    bool pressedTooEarly = false;
-    do
-    {
-      if (isJoystickUpPressed() || isJoystickDownPressed())
-      {
-        printf("Too soon!\n");
-        printf("\n");
-        pressedTooEarly = true;
-        break;
-      }
-    } while (getTimeInMs() - roundStartTime < waitTimeBeforeRoundStart);
-
-    if (pressedTooEarly)
+    if (playerPressedTooEarly())
     {
       continue;
     }
 
-    Direction direction = getRandomDirectionUpOrDown();
-    turnOnLED(direction);
-
-    long long startTime = getTimeInMs();
-
-    do
+    ROUND_RESULT roundResult = startRound();
+    if (roundResult == START_NEW_ROUND)
     {
-      if ((isJoystickUpPressed() && direction == UP) || (isJoystickDownPressed() && direction == DOWN))
-      {
-        long long responseTime = getTimeInMs() - startTime;
-
-        if (fastestTime == 0)
-        {
-          fastestTime = responseTime;
-        }
-        else if (fastestTime > responseTime)
-        {
-          fastestTime = responseTime;
-          printf("NEW RECORD! ");
-        }
-
-        printf("Response time: %lldms\n", responseTime);
-        printf("\n");
-        correctAnswerLEDFlash();
-        sleepForMs(CORRECT_ANSWER_LED_FLASH_DURATION_MS);
-
-        break;
-      }
-
-      if ((isJoystickUpPressed() && direction != UP) || (isJoystickDownPressed() && direction != DOWN))
-      {
-        printf("Whoops, incorrect.\n");
-        printf("\n");
-        incorrectAnswerLEDFlash();
-        sleepForMs(INCORRECT_ANSWER_LED_FLASH_DURATION_MS);
-
-        break;
-      }
-
-      if (isJoystickLeftPressed() || isJoystickRightPressed())
-      {
-        endGame();
-        return 0;
-      }
-    } while (getTimeInMs() - startTime <= MAX_RESPONSE_TIME_IN_MS);
-
-    if (getTimeInMs() - startTime > MAX_RESPONSE_TIME_IN_MS)
+      continue;
+    }
+    else if (roundResult == END_GAME)
     {
       endGame();
-      return 0;
+      break;
     }
   }
 
